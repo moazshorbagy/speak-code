d8b97cec79d2bf69e40343d0c4f9ac4108580fef
Moaz <moaz_elshorbagy@hotmail.com>
2020-02-22 05:02:08 +0200

    Tabs functionality added

--- js/codeslate.js
+++ js/codeslate.js
@@ -1,60 +1,49 @@
+let editor = null;
 
-var fileTabCount = 0;
-var openedTabs = 0;
-var untitledCount = 1;
-
-var editorId = '123';
-
-var opened = false;
-
-var editor = null;
+let untitledCount = 1;
 
 let sessions = []
 
-
 function createNewFile() {
 
-    untitledCount++;
-    openedTabs++;
-
-    editorId = "codeslate_" + fileTabCount;
-
     //responsible for appending a new list item in filesContainer
-    addNewExplorerTabInFilesConitainer(editorId);
+    addNewExplorerTabInFilesConitainer();
 
     //responsible for opening a coding screen
     openCodeSlate();
 
 }
 
-function addClickHandler(elem, editorId) {
+function addNewExplorerTabInFilesConitainer() {
+
+    let fileName = "Untitled - " + untitledCount++;
+    let sessionId = `session_${sessions.length}`
+
+    $(".filesContainer").append("<li id='" + sessionId + "'> <span class='closeTabIcon'>x</span> <div class='fileNameSpan'>" + fileName + "</div> </li> ");
+
+    let file = document.getElementById(sessionId);
+    addClickHandler(file, sessionId)
+}
+
+function addClickHandler(elem, sessionId) {
     elem.addEventListener('click', function () {
-        editor.setSession(parseInt(sessions[editorId[editorId.length - 1]]));
+        console.log(parseInt(sessionId.split('_')[1]))
+        editor.setSession(sessions[parseInt(sessionId.split('_')[1])]);
     }, false);
 }
 
-function addNewExplorerTabInFilesConitainer(editorId) {
-
-    var fileName = "Untitled - " + untitledCount;
-
-    $(".filesContainer").append("<li id='" + '$' + editorId + "'> <span class='closeTabIcon'>x</span> <div class='fileNameSpan'>" + fileName + "</div> </li> ");
-    var file = document.getElementById('$' + editorId);
-    addClickHandler(file, editorId)
-
-    // console.log(file);
-}
-
 function openCodeSlate() {
-    if (!opened) {
-        var editorStyles = "position:relative;" +
+    if (!sessions.length) {
+        const editorId = 'editorId';
+
+        let editorStyles = "position:relative;" +
             "top:0; right:0; bottom:90; left:0;" +
             "font-size:12pt; font-weight:normal; white-space:nowrap; display:block; z-index:999";
 
-        var editorDesign = "<div class='codeslate' id='" + editorId + "' style='" + editorStyles + "'></div>";
+        let editorDesign = "<div class='codeslate' id='" + editorId + "' style='" + editorStyles + "'></div>";
 
         $(".editorContainer").append(editorDesign);
         editor = ace.edit(editorId);
-        opened = true;
     }
 
     sessions.push(ace.createEditSession('', 'ace/mode/python'));
