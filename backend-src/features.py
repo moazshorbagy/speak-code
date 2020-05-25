c4b86fd67233ca8d0656d6c434a01a0e22886805
Moaz Alaa El-Shorbagy <moaz_elshorbagy@hotmail.com>
2020-05-26 00:54:14 +0200

    Bug fix in features.py

--- backend-src/features.py
+++ backend-src/features.py
@@ -23,7 +23,7 @@ def get_features(signal):
     mfcc                = mfcc[:,1:c.n_mfcc+1]
 
     # this part add context frames to the mfcc features
-    if add_context:
+    if c.add_context:
         dummy_array = np.zeros((c.n_context,c.n_mfcc),dtype=mfcc.dtype)
         mfcc_temp   = np.concatenate((dummy_array,mfcc,dummy_array),axis=0).ravel()
         mfcc        = np.lib.stride_tricks.as_strided(  mfcc_temp,
