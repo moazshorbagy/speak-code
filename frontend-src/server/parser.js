12ad90356f2b95888f8854dcd571f8f4a1b7975d
muhammad20 <muhammadahmad201196@hotmail.com>
2020-06-06 19:14:46 +0200

    variable naming is understood in preprocessing stage

--- frontend-src/server/parser.js
+++ frontend-src/server/parser.js
@@ -3,10 +3,6 @@ const commandParser = require('./commands-parser');
 
 var language = require('./language.json');
 
-let lastCommand;
-
-let currentCursorPosition;
-
 // a boolean to detect whether the last word was a command or not.
 let constructingCommand;
 
@@ -69,57 +65,136 @@ function isIndirectCommand(word) {
     }
 }
 
+function buildVariableName(words) {
+
+    wordsAfterCombiningVarName = [];
+
+    // the index of the first word after variable keyword
+    i = words.indexOf('variable') + 1;
+
+    for (k = 0; k < i - 1; k++) {
+        wordsAfterCombiningVarName.push(words[k]);
+    }
+
+    // if the user didnt say the case keyword
+    if (words.indexOf('case') < 0) {
+        return words;
+    }
+
+    // index of naming convetion desired by the user
+    j = words.indexOf('case') - 1;
+
+    switch (words[j]) {
+
+        case 'camel': {
+
+            varName = words[i];
+
+            for (k = i + 1; k < j; k++) {
+                varName += words[k].charAt(0).toUpperCase() + words[k].slice(1);
+            }
+            break;
+        }
+        case 'snake': {
+
+            varName = words[i];
+
+            for (k = i; k < j; k++) {
+                varName += "_" + words[k];
+            }
+            break;
+        }
+        case 'pascal': {
+            varName = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+            for (k = i + 1; k < j; k++) {
+                varName += words[k].charAt(0).toUpperCase() + words[k].slice(1);
+            }
+            break;
+        }
+        default: {
+            varName = words[i];
+            for (k = i + 1; k < j; k++) {
+                varName += words[k];
+            }
+            break;
+        }
+    }
+
+    //insert the variable name in the array as a whole string
+    wordsAfterCombiningVarName.push(varName);
+
+    for (k = j + 2; k < words.length; k++) {
+        wordsAfterCombiningVarName.push(words[k]);
+    }
+
+    return wordsAfterCombiningVarName;
+}
+
+
 module.exports = {
-    parseCommand: function (mainWindow, cmd) {
-        // if the user says cancel, the buffer of creating commands
-        // or code blocks is flushed.
-        if (cancelCommandOrCodeBlockCreation(cmd)) {
-            return;
+    parseCommand: function (mainWindow, words) {
+
+        //preprocessing the sentence
+        if(typeof words === 'string') {
+            words = words.split(' ');
         }
 
-        wordNotInGrammar = isWordNotInGrammar(cmd);
-        directCode = isDirectWordInsertion(cmd);
-        indicrectCode = isIndirectCodeInsertion(cmd);
+        // building variable name
+        if (words.includes('variable')) {
+            words = buildVariableName(words);
+            console.log(words);
+        }
 
-        if (commandIsBeingConstructed) {
-            if (constructingCommand) {
-                if (wordNotInGrammar) {
-                    commandParser.constructIndicrectCommand(mainWindow, cmd);
+        for (i = 0; i < words.length; i++) {
+
+            cmd = words[i];
+            // if the user says cancel, the buffer of creating commands
+            // or code blocks is flushed.
+            if (cancelCommandOrCodeBlockCreation(cmd)) {
+                continue;
+            }
+
+            wordNotInGrammar = isWordNotInGrammar(cmd);
+            directCode = isDirectWordInsertion(cmd);
+            indicrectCode = isIndirectCodeInsertion(cmd);
+
+            if (commandIsBeingConstructed) {
+                if (constructingCommand) {
+                    if (wordNotInGrammar) {
+                        commandParser.constructIndicrectCommand(mainWindow, cmd);
+                    }
+                } else {
+                    if (wordNotInGrammar) {
+                        codeParser.constructIndicrectCodeBlock(mainWindow, cmd);
+                    } else {
+                        if (directCode) {
+                            codeParser.constructIndicrectCodeBlock(mainWindow, directCode);
+                        } else if (indicrectCode) {
+                            codeParser.constructIndicrectCodeBlock(mainWindow, indicrectCode);
+                        }
+                    }
                 }
             } else {
+                directCommand = isDirectCommand(cmd);
+                indirectCommand = isIndirectCommand(cmd);
+
                 if (wordNotInGrammar) {
-                    codeParser.constructIndicrectCodeBlock(mainWindow, cmd);
-                } else {
-                    if (directCode) {
-                        codeParser.constructIndicrectCodeBlock(mainWindow, directCode);
-                    } else if (indicrectCode) {
-                        codeParser.constructIndicrectCodeBlock(mainWindow, indicrectCode);
+                    codeParser.insertDirectCode(mainWindow, cmd);
+                } else if (directCode) {
+                    codeParser.insertDirectCode(mainWindow, directCode);
+                } else if (indicrectCode) {
+                    if (codeParser.constructIndicrectCodeBlock(mainWindow, indicrectCode)) {
+                        commandIsBeingConstructed = false;
+                    }
+                } else if (directCommand) {
+                    commandParser.executeCommand(mainWindow, directCommand);
+                } else if (indirectCommand) {
+                    if (commandParser.constructIndicrectCommand(mainWindow, directCommand)) {
+                        commandIsBeingConstructed = false;
                     }
                 }
             }
-            return;
-        } else {
-            directCommand = isDirectCommand(cmd);
-            indirectCommand = isIndirectCommand(cmd);
-
-            if (wordNotInGrammar) {
-                codeParser.insertDirectCode(mainWindow, cmd);
-            } else if (directCode) {
-                codeParser.insertDirectCode(mainWindow, directCode);
-            } else if (indicrectCode) {
-                if (codeParser.constructIndicrectCodeBlock(mainWindow, indicrectCode)) {
-                    commandIsBeingConstructed = false;
-                }
-            } else if (directCommand) {
-                commandParser.executeCommand(mainWindow, directCommand);
-            } else if (indirectCommand) {
-                if (commandParser.constructIndicrectCommand(mainWindow, directCommand)) {
-                    commandIsBeingConstructed = false;
-                }
-            }
         }
-
-
     }
 }
 
