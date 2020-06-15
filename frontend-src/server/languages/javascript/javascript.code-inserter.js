f25c5c42752a233cf65a79f8b127515fc0a4bd32
muhammad20 <muhammadahmad201196@hotmail.com>
2020-06-15 04:51:28 +0200

    added javascript code inserter

---
+++ frontend-src/server/languages/javascript/javascript.code-inserter.js
@@ -0,0 +1,17 @@
+module.exports = class JavascriptCodeInserter {
+    getScope(line) {
+
+    }
+    
+    newScope() {
+    
+    }
+    
+    exitScope() {
+    
+    }
+    
+    enter() {
+        
+    }
+}
