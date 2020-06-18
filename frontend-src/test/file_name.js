d239562a808129e9323d14e113c6cc86362db363
Moaz Alaa El-Shorbagy <moaz_elshorbagy@hotmail.com>
2020-06-18 02:05:21 +0200

    Test cases added

--- frontend-src/test/file_name.js
+++
@@ -1,7 +0,0 @@
-const assert = require('assert');
-
-describe('file_name', function () {
-    it('description of testing a function', function () {
-        assert.equal([1, 2, 3].indexOf(4), -1);
-    });
-});
---
+++ frontend-src/test/parser.test.js
@@ -0,0 +1,86 @@
+const assert = require('assert');
+const parser = require('../server/parser');
+const pythonLang = require('../server/languages/python/python-lang.json');
+
+describe('Function: Form Numbers', function () {
+    it('Should convert string array of text into numbers', function () {
+        input = ['one', 'two', 'three'];
+        expectedOutput = 123;
+
+        actualOutput = parser.formNumbers(input);
+
+        assert.equal(actualOutput, expectedOutput, `Numbers are not equal: expected ${expectedOutput} but got ${actualOutput}.`);
+    });
+
+    it('Should throw error if input is not an array', function () {
+        input = 'one two three';
+
+        assert.throws(() => parser.formNumbers(input), TypeError, 'It did not throw TypeError as expected.');
+    });
+});
+
+
+describe('Function: Build Variable Name', function () {
+    it('Should convert string array to camel-case variable name', function () {
+        input = ['variable', 'my', 'new', 'variable', 'camel', 'case'];
+        expectedOutput = 'myNewVariable';
+
+        actualOutput = parser.buildVariableName(input);
+
+        assert.equal(actualOutput, expectedOutput, `Variable names are different: expected ${expectedOutput} but got ${actualOutput}.`);
+    });
+
+    it('Should convert string array to snake-case variable name', function () {
+        input = ['variable', 'my', 'new', 'variable', 'snake', 'case'];
+        expectedOutput = 'my_new_variable';
+
+        actualOutput = parser.buildVariableName(input);
+
+        assert.equal(actualOutput, expectedOutput, `Variable names are different: expected ${expectedOutput} but got ${actualOutput}.`);
+    });
+
+    it('Should convert string array to pascal-case variable name', function () {
+        input = ['variable', 'my', 'new', 'variable', 'pascal', 'case'];
+        expectedOutput = 'MyNewVariable';
+
+        actualOutput = parser.buildVariableName(input);
+
+        assert.equal(actualOutput, expectedOutput, `Variable names are different: expected ${expectedOutput} but got ${actualOutput}.`);
+    });
+
+    it('Should convert two variable names', function () {
+        input = ['variable', 'my', 'new', 'variable', 'pascal', 'case', 'variable', 'my', 'new', 'variable', 'camel', 'case'];
+        expectedOutput = 'MyNewVariable myNewVariable';
+
+        actualOutput = parser.buildVariableName(input);
+
+        assert.equal(actualOutput, expectedOutput, `Variable names are different: expected ${expectedOutput} but got ${actualOutput}.`);
+    });
+
+    it('Should throw error if input is not an array', function () {
+        input = 'variable my new variable snake case';
+
+        assert.throws(() => parser.buildVariableName(input), TypeError, 'It did not throw TypeError as expected.');
+    });
+});
+
+describe('Function: Process Sentence', function () {
+    it('Should convert multi-word commands to one word', function () {
+        input = ['else', 'if', 'bracket'];
+        expectedOutput = ['else-if', 'bracket'];
+
+        actualOutput = parser.processSentence(input, pythonLang);
+
+        assert.equal(actualOutput.length, expectedOutput.length, `Array lengths are not equal: expected ${expectedOutput.length} but got ${actualOutput.length}.`);
+
+        for (let i = 0; i < expectedOutput.length; i++) {
+            assert.equal(actualOutput[i], expectedOutput[i], `Elements not the same: expected ${expectedOutput.length} but got ${actualOutput.length}.`);
+        }
+    });
+
+    it('Should throw error if input is not an array', function () {
+        input = 'some random text';
+
+        assert.throws(() => parser.processSentence(input, pythonLang), TypeError, 'It did not throw TypeError as expected.');
+    });
+});
