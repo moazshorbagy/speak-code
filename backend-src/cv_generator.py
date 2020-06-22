4d5c4d51add5f04080e69920712723f2fb647ad0
Moaz Alaa El-Shorbagy <moaz_elshorbagy@hotmail.com>
2020-06-23 01:34:18 +0200

    Update cv_generator.py

--- backend-src/cv_generator.py
+++ backend-src/cv_generator.py
@@ -75,3 +75,41 @@ def generator_compiled(data_type='train', batch_size=c.batch_size):
         yield [X, y], y_lag
 
         i += 1
+
+
+def generator_compiled_no_pad(data_type='train', batch_size=c.batch_size):
+    data = pd.read_csv(os.path.join('CV_16', f'cv_{data_type}.csv')).to_numpy()
+    np.random.shuffle(data)
+    steps_per_epoch = data.shape[0] // batch_size
+    i = 0
+
+    while True:
+        if i==steps_per_epoch:
+            i = 0
+            np.random.shuffle(data)
+
+        batch_data = data[i*batch_size:(i+1)*batch_size]
+        
+        X = [''] * int(batch_size)
+        _y = [''] * int(batch_size)
+
+        for j, (path, transcript) in enumerate(batch_data):
+            X[j] = np.load(path + '.X.npy')
+            _y[j] = encode(c.start_token + transcript + c.end_token)
+
+        X = pad_sequences(X, padding='post', value=c.masking_value, dtype=np.float32)
+        _y = pad_sequences(_y, padding='post', value=encode(c.pad_token)[0], dtype=np.float32)
+
+        y = np.zeros((batch_size, _y.shape[1], c.n_output), dtype=np.float32)
+        
+        for j in range(batch_size):
+            y[j] = to_categorical(_y[j], num_classes=c.n_output)
+        
+        y_lag = y.copy()
+        y_lag = np.delete(y_lag, 0, 1)
+        pad = np.reshape([to_categorical(encode(c.pad_token)[0], num_classes=c.n_output)]*batch_size, (batch_size, 1, -1))
+        y_lag = np.append(y_lag, pad, 1)
+
+        yield [X, y], y_lag
+
+        i += 1
