6f72c73e1024cc247dcbaf7d3ff9a9b0e72fcc7c
muhammad20 <muhammadahmad201196@hotmail.com>
2020-06-24 23:58:34 +0200

    updated imports

--- frontend-src/import.js
+++ frontend-src/import.js
@@ -3,3 +3,4 @@ const wd = require('./files-handling/working-directory');
 const editor = require('./editor/editor');
 const openedFiles = require('./html-elements/open-editors');
 const collapsible = require('./html-elements/collapsible');
+const modelsEventEmitters = require('./editor/model-did-change-event');
