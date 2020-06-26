ed589d254cb84f1fe01d22ade26ddf9d838394ab
muhammad20 <muhammadahmad201196@hotmail.com>
2020-06-26 09:15:51 +0200

    functions take all previous lines instead of current line

--- frontend-src/server/languages/python/python.code-inserter.js
+++ frontend-src/server/languages/python/python.code-inserter.js
@@ -1,6 +1,6 @@
 module.exports = class PythonCodeInserter {
     getScope(currentLine) {
-        var k = 0;
+        let k = 0;
         for (let i = 0; i < currentLine.length; i++) {
             if (currentLine[i] == ' ') {
                 k += 1;
@@ -11,31 +11,36 @@ module.exports = class PythonCodeInserter {
         return k / 4;
     }
 
-    newScope(line) {
-        var code = ":\n";
+    newScope(lines) {
+        currentLine = lines[lines.length - 1];
+        
+        let code = ":\n";
     
-        var newScope = 4 * (this.getScope(line) + 1);
+        let newScope = 4 * (this.getScope(currentLine) + 1);
     
         code += (" ").repeat(newScope);
     
         return code;
     }
 
-    exitScope(line) {
-        var code = "\n";
+    exitScope(lines) {
+        currentLine = lines[lines.length - 1];
+
+        let code = "\n";
     
-        var newScope = Math.max(4 * (this.getScope(line) - 1), 0);
+        let newScope = Math.max(4 * (this.getScope(currentLine) - 1), 0);
     
         code += (" ").repeat(newScope);
     
         return code;
     }  
     
-    enter(line) {
+    enter(lines) {
+        currentLine = lines[lines.length - 1];
 
-        var code = "\n";
+        let code = "\n";
     
-        code += (" ").repeat(this.getScope(line) * 4);
+        code += (" ").repeat(this.getScope(currentLine) * 4);
     
         return code;
     }
